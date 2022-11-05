e8a09cfe134f73f0a985a7fbf18282f84e2b34ed
KAO HU WEI <will8327111@gmail.com>
2022-11-05 14:54:41 +0800

    Add Branch

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -3,8 +3,11 @@ package testApp;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
+		//組長新增的 CODE
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(5, 5));
 	}
 
 }
